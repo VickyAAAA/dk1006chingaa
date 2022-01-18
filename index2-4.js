f9125f803cd40aa6f159f2a0b96a17b033825ec9
VivkyAAAA <dk1006ching@gmail.com>
2022-01-18 10:57:32 +0800

    v0118_2

--- index2-4.js
+++ index2-4.js
@@ -1,14 +1,2 @@
-// x = 5
-//let x;
-//console.log(x);
-//x=5;
-//console.log(x);
-//x='小凡';
-//console.log(x);
-
-//let x = 5;
-//console.log(x);
-//let x = 6;
-
-const PI = 3.14159; //
-PI=5;
+let myName = '小凡';
+console.log(myName);
